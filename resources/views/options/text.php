4377cd760dd20a9af1dacd298979a72c7141c0fb
Vikas Bhavsar <vikasbhvsr@gmail.com>
2022-12-16 22:37:04 -0800

    Add css_class arg support to text field

--- resources/views/options/text.php
+++ resources/views/options/text.php
@@ -3,12 +3,14 @@
         <label for="<?php echo $option->get_id_attribute(); ?>"><?php echo $option->get_label(); ?></label>
     </th>
     <td class="forminp forminp-text">
-        <input name="<?php echo esc_attr($option->get_name_attribute()); ?>" id="<?php echo $option->get_id_attribute(); ?>" type="text" style="" value="<?php echo $option->get_value_attribute(); ?>" class="">
-        <?php if($description = $option->get_arg('description')) { ?>
+        <input name="<?php echo esc_attr($option->get_name_attribute()); ?>" id="<?php echo $option->get_id_attribute(); ?>" type="text" style="" value="<?php echo $option->get_value_attribute(); ?>" class="<?php if ($css_classes = $option->get_arg('css_classes')) {
+                                                                                                                                                                                                                    echo strtolower(sanitize_text_field($css_classes));
+                                                                                                                                                                                                                }; ?>">
+        <?php if ($description = $option->get_arg('description')) { ?>
             <p class="description"><?php echo $description; ?></p>
         <?php } ?>
 
-        <?php if($error = $option->has_error()) { ?>
+        <?php if ($error = $option->has_error()) { ?>
             <div class="wps-error-feedback"><?php echo $error; ?></div>
         <?php } ?>
     </td>
