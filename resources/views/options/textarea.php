2ec1a60a67f5388921ff76a66354e755e215207f
Jeffrey van Rossum <jeffrey@vanrossum.dev>
2023-08-09 15:54:24 +0200

    wip

--- resources/views/options/textarea.php
+++ resources/views/options/textarea.php
@@ -3,7 +3,12 @@
         <label for="<?php echo $option->get_id_attribute(); ?>" class="<?php echo $option->get_label_class_attribute(); ?>"><?php echo $option->get_label(); ?></label>
     </th>
     <td class="forminp forminp-text">
-        <textarea name="<?php echo esc_attr($option->get_name_attribute()); ?>" id="<?php echo $option->get_id_attribute(); ?>" class="<?php echo $option->get_input_class_attribute(); ?>"><?php echo $option->get_value_attribute(); ?></textarea>
+        <textarea
+            name="<?php echo esc_attr($option->get_name_attribute()); ?>"
+            id="<?php echo $option->get_id_attribute(); ?>"
+            class="<?php echo $option->get_input_class_attribute(); ?>"
+            rows="<?php echo $option->get_arg('rows', 5); ?>"
+            cols="<?php echo $option->get_arg('cols', 50); ?>"><?php echo $option->get_value_attribute(); ?></textarea>
         <?php if($description = $option->get_arg('description')) { ?>
             <p class="description"><?php echo $description; ?></p>
         <?php } ?>
